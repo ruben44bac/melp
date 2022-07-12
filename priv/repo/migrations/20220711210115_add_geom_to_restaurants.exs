93c1230be4a1b361d775755b95e971216094386c
Ruben Baeza <35118058+ruben44bac@users.noreply.github.com>
2022-07-12 08:56:11 -0500

    update migrate

--- priv/repo/migrations/20220711210115_add_geom_to_restaurants.exs
+++ priv/repo/migrations/20220711210115_add_geom_to_restaurants.exs
@@ -2,6 +2,10 @@ defmodule Melp.Repo.Migrations.AddGeomToRestaurants do
   use Ecto.Migration
 
   def change do
+
+
+    execute "CREATE EXTENSION IF NOT EXISTS postgis;"
+
     alter table(:restaurants) do
       add :geom,     :geometry
     end
