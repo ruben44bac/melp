c185888d5671c88e014a82604026686510810651
Ruben Baeza <35118058+ruben44bac@users.noreply.github.com>
2022-07-11 20:10:55 -0500

    update runtime

--- config/runtime.exs
+++ config/runtime.exs
@@ -31,10 +31,12 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
 
   config :melp, Melp.Repo,
-    # ssl: true,
+    ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+    socket_options: maybe_ipv6,
+    adapter: Ecto.Adapters.Postgres,
+    types: Melp.PostgresTypes
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
